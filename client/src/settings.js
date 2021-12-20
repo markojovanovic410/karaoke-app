1a0737883b164f894f88c8fe26f442b05f1aa70a
markojovanovic410 <markojovfreelancer@gmail.com>
2021-12-21 06:52:43 +0900

    first commit

---
+++ client/src/settings.js
@@ -0,0 +1,11 @@
+// Setting Variables
+const settings = {
+  serverIpAddress: '',
+  socketUrl: '',
+  //IF YOU HAVE BACKSLASH('\') IN YOUR PATH, YOU SHOULD REPLACE THIS TO DOUBLE BACKSLASH('\\').
+  // EXAMPLE: '\\\\sulling\\karaoke\\English Karaoke (MP4)'
+  songsPath: '',
+  songDelay: 10, // seconds,
+  pageSize: 1000 // page size
+}
+export default settings;
